14e5e9e45950c99e260129bdd584deb6b7e2586b
Aishwarya Jakka <aishwaryajakka@hotmail.com>
2020-04-17 12:41:23 -0400

    Added SQL connection

--- app.js
+++ app.js
@@ -5,23 +5,40 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mysql = require('mysql');
 
 var app = express();
 const port = 4000;
 
 // view engine setup
-app.engine('html',require('ejs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
+ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.set('view engine','html');
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
+
+/* SQL Database Connection*/
+
+var con = mysql.createConnection({
+  host: "localhost",
+  port: "8888",
+  user: "root",
+  password: "root",
+  database : "course_rec"
+});
+
+con.connect(function (err) {
+  if (err) throw "Connection to SQL failed";
+  console.log("Connected to SQL");
+  app.locals.con = con;
+});
+
 
 
 
