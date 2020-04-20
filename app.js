a6030b0c66a84ad0dddb18f61de4a595bfd17e09
Kenny Wu <Kenny@Kennys-MacBook-Pro.local>
2020-04-20 12:26:15 -0400

    edited router and mysql port

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var session = require('express-session');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var mysql = require('mysql');
+var createError = require("http-errors");
+var express = require("express");
+var session = require("express-session");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var indexRouter = require("./routes/index");
+var homeRouter = require("./routes/home");
+var usersRouter = require("./routes/users");
+var mysql = require("mysql");
 
 var app = express();
 const port = 4000;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 
@@ -20,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
 app.use(session({
-	secret: 'secret',
+	secret: "secret",
 	resave: true,
 	saveUninitialized: true
 }));
@@ -29,14 +30,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/home", homeRouter);
+app.use("/users", usersRouter);
+
 
 /* SQL Database Connection*/
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: "8889",
+  port: "7778",
   user: "root",
   password: "root",
   database : "course_rec",
@@ -52,7 +55,7 @@ connection.connect(function (err) {
   app.locals.connection = connection;
 });
 
-
+/*
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -62,13 +65,13 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
+*/
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
